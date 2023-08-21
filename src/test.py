b76a00fb18932c364eb12d23cceff63cf29e75d3
jiaxin-yeah <jiaxinbehappy@gmail.com>
2023-08-21 15:27:35 -0400

    added test

--- src/test.py
+++ src/test.py
@@ -2,12 +2,12 @@ from geodata import Dataset
 from geodata.model.wind import WindExtrapolationModel
 dataset = Dataset(
     module="merra2",
-    weather_data_config="slv_flux_hourly", # slv_flux_hourly
+    weather_data_config="slv_flux_hourly",
     years=slice(2010, 2010),
     months=slice(1,1)
 )
 dataset.get_data()
-#dataset.trim_variables()
 model = WindExtrapolationModel(dataset)
 model.prepare()
-model.estimate(height=12, xs=slice(1, 1), ys=slice(1, 1), years=slice(2010, 2010), months=slice(1, 1))
+result = model.estimate(height=12, xs=slice(1, 1), ys=slice(1, 1), years=slice(2010, 2010), months=slice(1, 1))
+print(result)
